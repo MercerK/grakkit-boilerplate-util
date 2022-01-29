3c168dacac0ca0ac448a993d01777a9ebd0f6704
Kyle <kyle.mercer.89@gmail.com>
2022-01-29 16:19:14 -0500

    v0.0.11

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { formatWebpackMessages } from './formatWebpackMessages'
+import { initializeAutoReload } from './initializeAutoReload'
+import { WebServer } from './webServer'
+
+export { WebServer }
+export { initializeAutoReload }
+export { formatWebpackMessages }
---
+++ src/initializeAutoReload.ts
@@ -0,0 +1,36 @@
+import { core } from '@grakkit/stdlib-paper'
+import { WebServer } from './webServer'
+
+/**
+ * This will initialize a webserver on port 4000 during development mode.
+ * When this receives a response at `/reload`, it'll start reloading the
+ * server.
+ *
+ * This is not recommended for production use.
+ */
+export function initializeAutoReload() {
+  WebServer.start()
+
+  if (process.env.NODE_ENV === 'development') {
+    WebServer.listen(4000)
+
+    WebServer.get('/reload', (req, res) => {
+      res.send('done')
+
+      reload()
+    })
+
+    console.log('Grakkit-Boilerplate-Util: Development Mode')
+  } else {
+    console.log('Grakkit-Boilerplate-Util: Production Mode')
+  }
+}
+
+function reload() {
+  if (process.env.NODE_ENV === 'development') {
+    WebServer.stop()
+  }
+
+  core.reload()
+  console.log('Grakkit-Boilerplate-Util: Reloaded')
+}
